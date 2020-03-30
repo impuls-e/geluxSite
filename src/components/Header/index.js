348aa564bd467f4343dc3ea99b5b97550fc7697f
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-30 15:55:43 -0300

    Create a responsive header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React from "react";
+
+import "./styles.css";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+import { MdEmail } from "react-icons/md";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
 
 export default function Header() {
+  const data = useStaticQuery(graphql`
+    query ImgHeader {
+      image: file(relativePath: { eq: "geluxlogo.png" }) {
+        childImageSharp {
+          fixed(height: 100) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
   return (
     <header className="container header">
-      <h1>Menu Superior</h1>
+      <Img fixed={data.image.childImageSharp.fixed} alt="Logo da Gelux" />
+      <div className="social-icons">
+        <a href="https://www.facebook.com/belabelindaapliques/">
+          <FaFacebookSquare color="#F9E233" />
+        </a>
+        <a href="https://www.instagram.com/belabelindabr/">
+          <FaInstagram color="#F9E233" />
+        </a>
+        <a href="https://www.instagram.com/belabelindabr/">
+          <MdEmail color="#F9E233" />
+        </a>
+      </div>
     </header>
-  )
+  );
 }
