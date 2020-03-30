1506e1ff1f2db4381c5691f5fde6d39b1e5feea9
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-30 17:56:46 -0300

    Developed Intro component

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,9 +1,28 @@
-import React from "react"
-
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import "./styles.css";
 export default function Intro() {
   return (
     <div className="container intro">
-      <h1>Intro</h1>
+      <h1>Soluções Completas em Elétrica</h1>
+      <div className="introduction">
+        <aside>
+          <p>Instalação e Manutenção Elétrica em Geral;</p>
+          <p>Instalação e Manutenção em Sistemas de Iluminação;</p>
+          <p>Projetos Elétricos e Luminotécnicos;</p>
+          <p>Projetos e Execução de Serviços CELESC;</p>
+          <p>Ensaios, Medições e Estudos Elétricos;</p>
+        </aside>
+        <nav>
+          <h2>Entre em Contato</h2>
+          <a href="">
+            <div className="whats">
+              <FaWhatsapp></FaWhatsapp>
+            </div>
+            <span>48 991815995</span>
+          </a>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
