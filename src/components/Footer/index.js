92813b80c9887657f7055b33d97d36ca00039dc0
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-16 19:09:59 -0300

    Add copyright impuls-e in page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles.css';
 export default function Footer() {
-  return (
-    <footer>
-      <p>Gostaria de fechar um projeto ?</p>
-      <a href='mailto:contato.gelux@gmail.com'>contato.gelux@gmail.com</a>
-    </footer>
-  );
+	return (
+		<>
+			<footer>
+				<p>Gostaria de fechar um projeto ?</p>
+				<a href='mailto:contato.gelux@gmail.com'>contato.gelux@gmail.com</a>
+			</footer>
+			<h3 id='impulse'>
+				Por | <a href='https://impuls-e.works'> Impuls-e</a>
+			</h3>
+		</>
+	);
 }
