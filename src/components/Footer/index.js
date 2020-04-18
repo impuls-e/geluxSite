abc64939857cb63cee5dc42e5ea14f88eeb0fe36
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-17 22:17:50 -0300

    Fix foot bug

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,6 @@ export default function Footer() {
 				<p>Gostaria de fechar um projeto ?</p>
 				<a href='mailto:contato.gelux@gmail.com'>contato.gelux@gmail.com</a>
 			</footer>
-			<h3 id='impulse'>
-				Por | <a href='https://impuls-e.works'> Impuls-e</a>
-			</h3>
 		</>
 	);
 }
